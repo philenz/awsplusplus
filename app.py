e17ef8cfe026075dbda86ce5e90a07232c8eba80
Phil Evans <phil.evans@gallagher.com>
2017-08-18 15:00:29 +1200

    import usual stuff, set up logging, cors

--- app.py
+++ app.py
@@ -1,29 +1,23 @@
-from chalice import Chalice
+import sys
+import os
+import json
+import boto3
+import logging
+
+from botocore.exceptions import ClientError
+from chalice import Chalice, BadRequestError, NotFoundError, Response
+
+if sys.version_info[0] == 3:
+    from urllib.parse import urlparse, parse_qs
+else:
+    from urlparse import urlparse, parse_qs
 
 app = Chalice(app_name='awsplusplus')
+app.debug = True
+app.log.setLevel(logging.DEBUG)
 
-
-@app.route('/')
+@app.route('/', cors=True)
 def index():
-    return {'hello': 'world'}
+    return {'hello': 'chalice'}
 
 
-# The view function above will return {"hello": "world"}
-# whenever you make an HTTP GET request to '/'.
-#
-# Here are a few more examples:
-#
-# @app.route('/hello/{name}')
-# def hello_name(name):
-#    # '/hello/james' -> {"hello": "james"}
-#    return {'hello': name}
-#
-# @app.route('/users', methods=['POST'])
-# def create_user():
-#     # This is the JSON body the user sent in their POST request.
-#     user_as_json = app.current_request.json_body
-#     # We'll echo the json body back to the user in a 'user' key.
-#     return {'user': user_as_json}
-#
-# See the README documentation for more examples.
-#
