3baad0961ebb90ff1540f26d220ba2da45eef35e
Phil Evans <phil.evans@gallagher.com>
2017-09-01 12:41:43 +1200

    correct user pool arn

--- chalicelib/constants.py
+++ chalicelib/constants.py
@@ -1,1 +1,1 @@
-POOL_ARN = ('arn:aws:cognito-idp:ap-southeast-2:092585690455:userpool/ap-southeast-2_Z27DhDe5N')
+POOL_ARN = ('arn:aws:cognito-idp:ap-southeast-2:092585690455:userpool/ap-southeast-2_Gbjvx5EuE')
